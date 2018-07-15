28f78aa9dd06cd03508a5dc2303e3be239904c17
clown <wangyw.zt@gmail.com>
2018-07-15 08:18:50 +0800

    add captcha

---
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\CaptchasRequest;
+use Gregwar\Captcha\CaptchaBuilder;
+use Illuminate\Http\Request;
+
+class CaptchasController extends Controller
+{
+    public function store(CaptchasRequest $request, CaptchaBuilder $captchaBuilder)
+    {
+        $phone = $request->phone;
+        $captcha = $captchaBuilder->build();
+        $key = 'captcha-' . str_random(15);
+        $expiresAt = now()->addMinutes(2);
+        \Cache::put($key, ['phone' => $phone, 'captcha' => $captcha->getPhrase()], $expiresAt);
+
+        $result = [
+            'captcha_key' => $key,
+            'expires_at' => $expiresAt->toDateTimeString(),
+            'captcha_image_content' => $captcha->inline()
+        ];
+        return $this->response->array($result)->setStatusCode(201);
+    }
+}
--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -17,19 +17,28 @@ class VerificationCodesController extends Controller
     public function store(VerificationCodeRequest $request, EasySms $sms)
     {
 
-        $phone = $request->phone;
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        $captchaData = \Cache::get($request->captcha_key);
+        if (!$captchaData) {
+            return $this->response->error('图片验证码失效', 422);
+        }
+        if (!hash_equals($captchaData['captcha'], $request->captcha_code)) {
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('图片验证码错误');
+        }
+        $phone = $captchaData['phone'];
         if (!app()->environment('production')) {
             $code = '1234';
         } else {
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
             try {
                 $result = $sms->send($phone, [
                     'content' => "【王跃武】您的验证码是{$code}。如非本人操作，请忽略本短信 "
                 ]);
             } catch (InvalidArgumentException $e) {
             } catch (NoGatewayAvailableException $e) {
-                $message = $e->getException('yunpian')->getMessage();
-                return $this->response->errorInternal($message ?? '短信发送异常');
+                $response = $e->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
             }
         }
 
@@ -38,6 +47,7 @@ class VerificationCodesController extends Controller
 
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
 
+        \Cache::forget($request->captcha_key);
         return $this->response->array([
             'key' => $key,
             'expired_at' => $expiredAt->toDateTimeString()
