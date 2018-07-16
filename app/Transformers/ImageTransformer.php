46690dc2bea47eab99a125f337dd545e18a4a93c
clown <wangyw.zt@gmail.com>
2018-07-16 21:38:50 +0800

    user update

---
+++ app/Transformers/ImageTransformer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clown
+ * Date: 7/16/18
+ * Time: 8:55 PM
+ */
+
+namespace App\Transformers;
+
+
+use App\Models\Image;
+use League\Fractal\TransformerAbstract;
+
+class ImageTransformer extends TransformerAbstract
+{
+    public function transform(Image $image)
+    {
+        return [
+            'id' => $image->id,
+            'user_id' => $image->user_id,
+            'type' => $image->type,
+            'path' => $image->path,
+            'created_at' => $image->created_at->toDateTimeString(),
+            'updated_at' => $image->updated_at->toDateTimeString(),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -55,7 +55,13 @@ $api->version('v1', [
 
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {
+            // 当前用户信息
             $api->get('user', 'UsersController@me')->name('api.user.show');
+            // 更改用户信息
+            $api->patch('user', 'UsersController@update')->name('api.user.update');
+            // 图片资源
+            $api->post('images', 'ImagesController@store')->name('api.images.store');
+
         });
     });
 });
