199f120bf51b8b7421e997e0fbf823cd1b8f67b3
clown <wangyw.zt@gmail.com>
2018-07-18 01:11:22 +0800

    notifications stats

--- routes/api.php
+++ routes/api.php
@@ -84,6 +84,8 @@ $api->version('v1', [
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')->name('api.topics.replies.destroy');
             // 消息通知列表
             $api->get('user/notifications', 'NotificationsController@index')->name('api.user.notifications.index');
+            // 未读消息数量
+            $api->get('user/notifications/stats', 'NotificationsController@stats')->name('api.user.notifications.stats');
         });
     });
 });
